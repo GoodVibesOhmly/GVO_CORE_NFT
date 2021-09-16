7992879230d07c263ea9a066fe33df7605851523
Eugenio Paluello <eugypalu@gmail.com>
2021-09-17 00:38:05 +0200

    WIP

--- resources/itemsv2.js
+++ resources/itemsv2.js
@@ -4,92 +4,161 @@ var mainInterfaceAddress = "0x915A22A152654714FcecA3f4704fCf6bd314624c";
 var dynamicUriResolverAddress;
 var ItemProjectionFactoryContract;
 var NativeProjection;
+var mainInterface;
 
-async function deploy(host, plainUri) {
+async function deployNativeProjection(nativeProjectionAddress = utilities.voidEthereumAddress) {
+  await blockchainConnection.init;
+
+  var MainInterface = await compile("model/IItemMainInterface");
+  mainInterface = new web3.eth.Contract(
+    MainInterface.abi,
+    mainInterfaceAddress
+  );
   
-    await blockchainConnection.init;
-
-    NativeProjection = await compile(
-      "projection/native/NativeProjection"
-    );
+  if(nativeProjectionAddress == utilities.voidEthereumAddress){
+    NativeProjection = await compile("projection/native/NativeProjection");
     var nativeProjectionContract = await new web3.eth.Contract(
       NativeProjection.abi
     )
       .deploy({ data: NativeProjection.bin, arguments: ["0x"] })
       .send(blockchainConnection.getSendingOptions());
 
-    var MainInterface = await compile('model/IItemMainInterface');
-    var mainInterface = new web3.eth.Contract(MainInterface.abi, mainInterfaceAddress);
-
-    dynamicUriResolverAddress = await mainInterface.methods.dynamicUriResolver().call();
-
     var model = nativeProjectionContract.options.address;
 
-    var ItemProjectionFactory = await compile(
-      "projection/factory/impl/ItemProjectionFactory"
-    );
-
-    var dataParam = web3.eth.abi.encodeParameters(
-      ["address"],
-      [mainInterfaceAddress]
-    );
-
-    dataParam = web3.eth.abi.encodeParameters(
-      ["address", "bytes"],
-      [model, dataParam]
-    );
-
-    dataParam = web3.eth.abi.encodeParameters(
-      ["string", "address", "bytes"],
-      [plainUri, dynamicUriResolverAddress, dataParam]
-    );
-
-    dataParam = web3.eth.abi.encodeParameters(
-      ["address", "bytes"],
-      [host, dataParam]
-    );
-
-    ItemProjectionFactoryContract = await new web3.eth.Contract(
-      ItemProjectionFactory.abi
-    )
-      .deploy({ data: ItemProjectionFactory.bin, arguments: [dataParam] })
-      .send(blockchainConnection.getSendingOptions());
+    return model;
   }
+  return nativeProjectionAddress
+}
+
+async function deploy(host, plainUri, nativeProjectionAddress = utilities.voidEthereumAddress) {
+
+  var model = await deployNativeProjection(nativeProjectionAddress)
+
+  dynamicUriResolverAddress = await mainInterface.methods
+    .dynamicUriResolver()
+    .call();
+
+  var ItemProjectionFactory = await compile(
+    "projection/factory/impl/ItemProjectionFactory"
+  );
+
+  var dataParam = web3.eth.abi.encodeParameters(
+    ["address"],
+    [mainInterfaceAddress]
+  );
+
+  dataParam = web3.eth.abi.encodeParameters(
+    ["address", "bytes"],
+    [model, dataParam]
+  );
+
+  dataParam = web3.eth.abi.encodeParameters(
+    ["string", "address", "bytes"],
+    [plainUri, dynamicUriResolverAddress, dataParam]
+  );
+
+  dataParam = web3.eth.abi.encodeParameters(
+    ["address", "bytes"],
+    [host, dataParam]
+  );
+
+  ItemProjectionFactoryContract = await new web3.eth.Contract(
+    ItemProjectionFactory.abi
+  )
+    .deploy({ data: ItemProjectionFactory.bin, arguments: [dataParam] })
+    .send(blockchainConnection.getSendingOptions());
+
+  return model;
+}
+
+async function initialization(
+  zeroDecimals,
+  collectionId,
+  header,
+  item,
+  host,
+  plainUri,
+  nativeProjectionAddress = utilities.voidEthereumAddress
+) {
+  await deploy(host, plainUri, nativeProjectionAddress);
+
+  var isDecimals = zeroDecimals;
+
+  var deployParam = abi.encode(["bool"], [isDecimals]);
+
+  deployParam = abi.encode(
+    [
+      "bytes32",
+      "tuple(address,string,string,string)",
+      "tuple(tuple(address,string,string,string),bytes32,uint256,address[],uint256[])[]",
+      "bytes",
+    ],
+    [collectionId, header, item, deployParam]
+  );
+
+  deployParam = abi.encode(["address", "bytes"], [host, deployParam]);
+
+  var transaction = await ItemProjectionFactoryContract.methods
+    .deploy(deployParam)
+    .send(blockchainConnection.getSendingOptions());
+
+  return new web3.eth.Contract(
+    NativeProjection.abi,
+    transaction.events.Deployed.returnValues.deployedAddress
+  );
+}
+
+async function createCollection(host, itemsToMint) {
+  var collection = {
+    host,
+    name: "Collection",
+    symbol: "COL",
+    uri: "uri",
+  };
+  var items = !itemsToMint
+    ? []
+    : itemsToMint.map((it, i) => {
+        return {
+          header: {
+            host,
+            name: "Item_" + i,
+            symbol: "IT_" + i,
+            uri: "URI_" + i,
+          },
+          collectionId: utilities.voidBytes32,
+          id: 0,
+          accounts: Object.keys(it),
+          amounts: Object.values(it),
+        };
+      });
+  var transaction = await mainInterface.methods
+    .createCollection(collection, items)
+    .send(blockchainConnection.getSendingOptions());
+  var logs = (await web3.eth.getTransactionReceipt(transaction.transactionHash))
+    .logs;
+  var collectionId = web3.eth.abi.decodeParameter(
+    "bytes32",
+    logs.filter(
+      (it) =>
+        it.topics[0] === web3.utils.sha3("Collection(address,address,bytes32)")
+    )[0].topics[3]
+  );
+  var itemIds = logs
+    .filter(
+      (it) =>
+        it.topics[0] ===
+        web3.utils.sha3("CollectionItem(bytes32,bytes32,uint256)")
+    )
+    .map((it) => web3.eth.abi.decodeParameter("uint256", it.topics[3]));
+  return {
+    collectionId,
+    itemIds,
+  };
+}
 
 module.exports = {
-
-    async initialization(zeroDecimals, collectionId, header, item, host, plainUri){
-
-        await deploy(host, plainUri);
-
-        var isDecimals = zeroDecimals;
-
-        var deployParam = abi.encode(["bool"], [isDecimals]);
-
-        deployParam = abi.encode(
-          [
-            "bytes32",
-            "tuple(address,string,string,string)",
-            "tuple(tuple(address,string,string,string),bytes32,uint256,address[],uint256[])[]",
-            "bytes"
-          ],
-          [
-            collectionId,
-            header,
-            item,
-            deployParam
-          ]
-        );
-
-        deployParam = abi.encode(
-          ["address", "bytes"],
-          [host, deployParam]
-        );
-
-      var transaction = await ItemProjectionFactoryContract.methods
-        .deploy(deployParam)
-        .send(blockchainConnection.getSendingOptions());
-
-      return new web3.eth.Contract(NativeProjection.abi, transaction.events.Deployed.returnValues.deployedAddress);
-    }
+  initialization,
+  deployNativeProjection,
+  deploy,
+  createCollection,
 };
--- test/itemv2-projections-native.js
+++ test/itemv2-projections-native.js
@@ -1,6 +1,12 @@
 var itemsv2 = require('../resources/itemsv2');
 
+var MainInterface
+var mainInterface
 describe("Item V2 Projections - Native", () => {
+  before(async () => {
+    MainInterface = await compile('model/IItemMainInterface');
+    mainInterface = new web3.eth.Contract(MainInterface.abi, knowledgeBase.mainInterfaceAddress);
+  });
   it("#0", async () => {
     var zeroDecimals = false;
     var collectionId = utilities.voidBytes32;
@@ -13,8 +19,103 @@ describe("Item V2 Projections - Native", () => {
     try {
       var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, accounts[1], "URI");
       console.log("Native", native.options.address);
+      assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
     } catch(e) {
       console.error(e);
     }
   });
+
+  it("#619 Alternative Initialization", async () => {
+    
+    var zeroDecimals = false;
+    var collectionId = utilities.voidBytes32;
+
+    var nativeProjectionAddress = await itemsv2.deployNativeProjection();
+
+    var items = [];
+
+    var collectionId = (await itemsv2.createCollection(nativeProjectionAddress, items))['collectionId']
+    var zeroDecimals = false;
+    var collectionId = utilities.voidBytes32;
+
+    var collectionHeader = [nativeProjectionAddress, 'Collection', 'COL', 'uri'];
+
+    var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, nativeProjectionAddress, "URI", nativeProjectionAddress);
+    assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
+  });
+
+  it("#620 Change Collection Metadata", async () => {
+    var zeroDecimals = false;
+    var collectionId = utilities.voidBytes32;
+
+    var collectionHeader = [accounts[1], 'Collection1', 'COL1', 'uri1'];
+    var newCollectionHeader = [accounts[1], 'Collection2', 'COL2', 'uri2'];
+
+    var items = [
+      [[utilities.voidEthereumAddress, '', '', ''], collectionId, 0, [accounts[1]], [10000]]
+    ];
+    var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, accounts[1], "URI");
+    
+    console.log("Native", native.options.address);
+    assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
+    console.log(await native.methods.collectionId().call())
+    console.log(await native.methods.mainInterface().call())
+    console.log(await native.methods.setHeader(newCollectionHeader).send(blockchainConnection.getSendingOptions({ from: accounts[1] })));
+  });
+
+  // it("#621 Change Collection Metadata", async () => {
+  //   var zeroDecimals = false;
+  //   var collectionId = utilities.voidBytes32;
+
+  //   var collectionHeader = [accounts[1], 'Collection1', 'COL1', 'uri1'];
+  //   var newCollectionHeader = [accounts[2], 'Collection2', 'COL2', 'uri2'];
+
+  //   var items = [
+  //     [[utilities.voidEthereumAddress, '', '', ''], collectionId, 0, [accounts[1]], [10000]]
+  //   ];
+  //   var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, accounts[1], "URI");
+    
+  //   console.log("Native", native.options.address);
+  //   assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
+
+  //   console.log(native.methods.setHeader(newCollectionHeader).send(blockchainConnection.getSendingOptions({ from: accounts[1] })));
+  // });
+
+  // it("#622 Change Collection Metadata", async () => {
+  //   var zeroDecimals = false;
+  //   var collectionId = utilities.voidBytes32;
+
+  //   var collectionHeader = [accounts[1], 'Collection1', 'COL1', 'uri1'];
+
+  //   var items = [
+  //     [[utilities.voidEthereumAddress, '', '', ''], collectionId, 0, [accounts[1]], [10000]]
+  //   ];
+  //   var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, accounts[1], "URI");
+  //   var newCollectionHeader = [native.options.address, 'Collection2', 'COL2', 'uri2'];
+
+  //   console.log("Native", native.options.address);
+  //   assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
+
+  //   console.log(native.methods.setHeader(newCollectionHeader).send(blockchainConnection.getSendingOptions({ from: accounts[1] })));
+  //   await mainInterface.methods.setCollectionsMetadata([collection], [newCollectionHeader]).send(blockchainConnection.getSendingOptions({ from: accounts[1] }));
+
+  // });
+
+  // it("#623 Change Collection Metadata", async () => {
+  //   var zeroDecimals = false;
+  //   var collectionId = utilities.voidBytes32;
+
+  //   var collectionHeader = [accounts[1], 'Collection1', 'COL1', 'uri1'];
+  //   var newCollectionHeader = [accounts[2], 'Collection2', 'COL2', 'uri2'];
+
+  //   var items = [
+  //     [[utilities.voidEthereumAddress, '', '', ''], collectionId, 0, [accounts[1]], [10000]]
+  //   ];
+  //   var native = await itemsv2.initialization(zeroDecimals, collectionId, collectionHeader, items, accounts[1], "URI");
+    
+  //   console.log("Native", native.options.address);
+  //   assert.equal(await native.methods.decimals(0).call(), zeroDecimals ? '0' : '18');
+
+  //   console.log(native.methods.setItemsMetadata(newCollectionHeader).send(blockchainConnection.getSendingOptions({ from: accounts[1] })));
+  // });
 });

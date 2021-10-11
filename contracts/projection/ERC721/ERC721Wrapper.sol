d2bd94523f43bfa8fb026ea2735a7513628284f2
Marco Vasapollo <ceo@metaring.com>
2021-10-11 13:16:59 +0300

    ERC721 - Mint

--- contracts/projection/ERC721/ERC721Wrapper.sol
+++ contracts/projection/ERC721/ERC721Wrapper.sol
@@ -23,8 +23,24 @@ contract ERC721Wrapper is IERC721Wrapper, ItemProjection, IERC721Receiver {
         return _itemIdOf[_toItemKey(tokenAddress, tokenId)];
     }
 
-    function mintItems(CreateItem[] calldata) virtual override(Item, ItemProjection) external returns(uint256[] memory) {
-        revert("You need to send ERC721 token or call proper mint function");
+    function mintItems(CreateItem[] calldata createItemsInput) virtual override(Item, ItemProjection) external returns(uint256[] memory itemIds) {
+        CreateItem[] memory createItems = new CreateItem[](createItemsInput.length);
+        uint256[] memory preloadedTokenIds = new uint256[](createItemsInput.length);
+        string memory uri = plainUri();
+        for(uint256  i = 0; i < createItemsInput.length; i++) {
+            address tokenAddress = address(uint160(uint256(createItemsInput[i].collectionId)));
+            uint256 tokenId = createItemsInput[i].id;
+            IERC721(tokenAddress).transferFrom(msg.sender, address(this), tokenId);
+            createItems[i] = _buildCreateItem(tokenAddress, createItemsInput[i].accounts, createItemsInput[i].amounts, preloadedTokenIds[i] = itemIdOf(tokenAddress, tokenId), uri);
+        }
+        itemIds = IItemMainInterface(mainInterface).mintItems(createItems);
+        for(uint256 i = 0; i < createItemsInput.length; i++) {
+            if(preloadedTokenIds[i] == 0) {
+                address tokenAddress = address(uint160(uint256(createItemsInput[i].collectionId)));
+                uint256 tokenId = createItemsInput[i].id;
+                emit Token(tokenAddress, tokenId, _itemIdOf[_toItemKey(tokenAddress, tokenId)] = itemIds[i]);
+            }
+        }
     }
 
     function onERC721Received(
@@ -39,29 +55,13 @@ contract ERC721Wrapper is IERC721Wrapper, ItemProjection, IERC721Receiver {
         }
         itemReceiver = itemReceiver != address(0) ? itemReceiver : from;
         uint256 itemId = itemIdOf(msg.sender, tokenId);
-        uint256 createdItemId = IItemMainInterface(mainInterface).mintItems(_buildCreateItems(msg.sender, tokenId, itemReceiver, itemId))[0];
+        CreateItem[] memory createItems = new CreateItem[](1);
+        createItems[0] = _buildCreateItem(msg.sender, itemReceiver.asSingletonArray(), new uint256[](0), itemId, plainUri());
+        uint256 createdItemId = IItemMainInterface(mainInterface).mintItems(createItems)[0];
         if(itemId == 0) {
             emit Token(msg.sender, tokenId, _itemIdOf[_toItemKey(msg.sender, tokenId)] = createdItemId);
         }
         return this.onERC721Received.selector;
-    }
-
-    function mint(address[] calldata tokenAddresses, uint256[] calldata tokenIds, address[] calldata receivers) override external returns(uint256[] memory itemIds) {
-        address defaultReceiver = msg.sender;
-        if(receivers.length == 1) {
-            defaultReceiver = receivers[0];
-        }
-        defaultReceiver = defaultReceiver != address(0) ? defaultReceiver : msg.sender;
-        itemIds = new uint256[](tokenIds.length);
-        for(uint256  i = 0 ; i < itemIds.length; i++) {
-            IERC721(tokenAddresses[i]).transferFrom(msg.sender, address(this), tokenIds[i]);
-            address itemReceiver = receivers.length <= 1 ? defaultReceiver : receivers[i];
-            itemIds[i] = itemIdOf(tokenAddresses[i], tokenIds[i]);
-            uint256 createdItemId = IItemMainInterface(mainInterface).mintItems(_buildCreateItems(tokenAddresses[i], tokenIds[i], itemReceiver != address(0) ? itemReceiver : defaultReceiver, itemIds[i]))[0];
-            if(itemIds[i] == 0) {
-                emit Token(tokenAddresses[i], tokenIds[i], itemIds[i] = _itemIdOf[_toItemKey(tokenAddresses[i], tokenIds[i])] = createdItemId);
-            }
-        }
     }
 
     function burn(address account, uint256 itemId, uint256 amount, bytes memory data) override(Item, ItemProjection) public {
@@ -102,13 +102,21 @@ contract ERC721Wrapper is IERC721Wrapper, ItemProjection, IERC721Receiver {
         token.safeTransferFrom(address(this), receiver, tokenId);
     }
 
-    function _buildCreateItems(address tokenAddress, uint256 tokenId, address receiver, uint256 itemId) private view returns(CreateItem[] memory createItems) {
-        (string memory name, string memory symbol, string memory uri) = itemId != 0 ? ("", "", "") : _tryRecoveryMetadata(tokenAddress, tokenId);
-        createItems = new CreateItem[](1);
-        createItems[0] = CreateItem(Header(address(0), name, symbol, uri), collectionId, itemId, receiver.asSingletonArray(), (1e18 - (itemId == 0 ? 0 : IItemMainInterface(mainInterface).totalSupply(itemId))).asSingletonArray());
+    function _buildCreateItem(address tokenAddress, address[] memory receivers, uint256[] memory values, uint256 itemId, string memory uri) private view returns(CreateItem memory) {
+        (string memory name, string memory symbol) = itemId != 0 ? ("", "") : _tryRecoveryMetadata(tokenAddress);
+        uint256 supplyToMint = (1e18 - (itemId == 0 ? 0 : IItemMainInterface(mainInterface).totalSupply(itemId)));
+        address[] memory accounts = receivers.length == 0 ? msg.sender.asSingletonArray() : receivers;
+        uint256[] memory amounts = values.length == 0 ? supplyToMint.asSingletonArray() : values;
+        require(accounts.length == amounts.length, "length");
+        for(uint256 i = 0; i < amounts.length; i++) {
+            require(accounts[i] != address(0), "zero address");
+            supplyToMint -= amounts[i];
+        }
+        require(supplyToMint == 0, "amount");
+        return CreateItem(Header(address(0), name, symbol, itemId != 0 ? "" : uri), collectionId, itemId, accounts, amounts);
     }
 
-    function _tryRecoveryMetadata(address source, uint256 tokenId) private view returns(string memory name, string memory symbol, string memory uri) {
+    function _tryRecoveryMetadata(address source) private view returns(string memory name, string memory symbol) {
         IERC721Metadata nft = IERC721Metadata(source);
         try nft.name() returns(string memory n) {
             name = n;
@@ -117,11 +125,6 @@ contract ERC721Wrapper is IERC721Wrapper, ItemProjection, IERC721Receiver {
         try nft.symbol() returns(string memory s) {
             symbol = s;
         } catch {
-        }
-        try nft.tokenURI(tokenId) returns(string memory s) {
-            uri = s;
-        } catch {
-            uri = super.uri();
         }
         if(keccak256(bytes(name)) == keccak256("")) {
             name = source.toString();
--- contracts/projection/ERC721/IERC721Wrapper.sol
+++ contracts/projection/ERC721/IERC721Wrapper.sol
@@ -10,6 +10,4 @@ interface IERC721Wrapper is IItemProjection {
     event Token(address indexed tokenAddress, uint256 indexed tokenId, uint256 indexed itemId);
 
     function itemIdOf(address tokenAddress, uint256 tokenId) external view returns(uint256);
-
-    function mint(address[] calldata tokenAddresses, uint256[] calldata tokenIds, address[] calldata receivers) external returns(uint256[] memory itemIds);
 }
